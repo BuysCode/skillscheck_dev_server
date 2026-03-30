86b1cbbb9203655531455b3da551b0e28c2c375d
Guilherme Buys <guibuys55@gmail.com>
2026-03-30 00:50:06 -0300

    feat: some changes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import router from './routes.js';
+import router from './routes';
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
 
 const app = express();
-const port = process.env.PORT!;
+const port = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(cookieParser());
